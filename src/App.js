14bbe3c57d8e91b4afa08557aa4f5c07d152ab56
its-gio <me@its-g.io>
2018-12-03 21:53:43 -0800

    Simple input checkboxes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">Testing</header>
-      </div>
+      <form>
+        <input type="checkbox" />
+        <span>Milk</span>
+        <input type="checkbox" />
+        <span>Milk</span>
+        <input type="checkbox" />
+        <span>Milk</span>
+      </form>
     );
   }
 }
