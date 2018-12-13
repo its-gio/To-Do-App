9c9e27d35b0d9efc573eaa7c3c9f7d14621a84ba
its-gio <me@its-g.io>
2018-12-13 13:38:58 -0800

    adding todoList json to state in app component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import TodoItem from "./components/TodoItem";
 import todoList from "./data/data.js";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todoList
+    };
+  }
   render() {
-    const todoItems = todoList.map(item => (
+    const todoItems = this.state.todos.map(item => (
       <TodoItem key={item.id} text={item.text} completed={item.completed} />
     ));
 
