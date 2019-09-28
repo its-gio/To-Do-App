b495df5f742a35b2a32e2b736c7d020cd8efcf99
Giovani Aguirre <me@its-g.io>
2019-09-27 22:50:28 -0700

    Added styles to newly created elements (task list items) and checked for blank space entries

--- scripts.js
+++ scripts.js
@@ -3,3 +3,37 @@ const NTInput = newTask.querySelector("input");
 const clearTasks = document.querySelector("#clear");
 const filter = document.querySelector(".tasks-area--filter input");
 const taskList = document.querySelector("#collection");
+
+newSubmit();
+
+// Get value of NTInput when newTask is submitted
+function newSubmit() {
+  newTask.addEventListener("submit", createListItem);
+}
+
+function createListItem(e) {
+  e.preventDefault();
+
+  // Check if input is empty
+  const inputVal = NTInput.value;
+  if (inputVal.trim() === "") { return };
+
+
+  // Create div
+  const div = document.createElement("div");
+  div.setAttribute("class", "task-Item");
+
+  // Create p
+  const p = document.createElement("p")
+
+  // Create text node for input value
+  const input = document.createTextNode(`${inputVal}`);
+
+  // Add everything togther
+  p.appendChild(input)
+  div.appendChild(p)
+  taskList.appendChild(div);
+
+  // Empty new task input
+  NTInput.value = "";
+}
