c03f54c24cd773f69c401ffe50944e1f15191d74
Giovani Aguirre <me@its-g.io>
2019-09-27 21:56:21 -0700

    Query seleecting all needed DOM elements and changing html classes to IDs

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const newTask = document.querySelector(".new-task");
+const NTInput = newTask.querySelector("input");
+const clearTasks = document.querySelector("#clear");
+const filter = document.querySelector(".tasks-area--filter input");
+const taskList = document.querySelector("#collection");
