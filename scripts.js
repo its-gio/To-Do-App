decfcefd2f204d785fb9b39f7cf98af15435c160
Giovani Aguirre <me@its-g.io>
2019-09-29 20:07:52 -0700

    Changed task-Item to task-item and added filter (Adds remove class)

--- scripts.js
+++ scripts.js
@@ -11,6 +11,10 @@ function newSubmit() {
   newTask.addEventListener("submit", createListItem);
 
   clearBtn.addEventListener("click", clearList);
+
+  filter.addEventListener("keyup", filterTasks);
+
+  filter.parentElement.addEventListener("submit", (e) => { e.preventDefault() });
 }
 
 function createListItem(e) {
@@ -22,10 +26,11 @@ function createListItem(e) {
 
   // Create div
   const div = document.createElement("div");
-  div.setAttribute("class", "task-Item");
+  div.classList.add("task-item");
 
   // Create p
   const p = document.createElement("p")
+  p.classList.add("task-item--content");
 
   // Create recycle link
   const a = document.createElement("a")
@@ -49,8 +54,20 @@ function recycleItem(e) {
 function clearList() {
   if (taskList.firstChild !== null) {
     while(taskList.firstChild !== null) { 
-      console.log(taskList.firstChild)
       taskList.firstChild.remove();
     } 
   } 
+}
+
+function filterTasks(e) {
+  const taskContent = taskList.querySelectorAll(".task-item");
+  const filterInput = e.target.value;
+
+  taskContent.forEach(task => {
+    if (task.firstChild.textContent.indexOf(filterInput) !== -1) {
+      task.classList.remove("remove");
+    } else {
+      task.classList.add("remove");
+    }
+  })
 }
